c4dc771818ddb8e4c2b9f1c630bdf292a8586bc8
tatiana <yarova_89@list.ru>
2023-09-10 18:15:53 +0300

    add tasks

---
+++ script.js
@@ -0,0 +1,27 @@
+// Задание 1
+let a = Number(10);
+alert(a);
+a = Number(20);
+alert(a);
+//Задание 2
+let yearIssue = Number(2007);
+alert(`Год Выпуска первого iPhone: ${yearIssue}`);
+//Задание 3
+let authorJavaScript = String(`Брендан Эйх`);
+alert(`Имя создателя языка JavaScript: ${authorJavaScript}`);
+//Задание 4
+let one = Number(10);
+let two = Number(2);
+let sum = (one + two);
+alert(`Сумма: ${sum}`);
+let difference = (one - two);
+alert(`Разность: ${difference}`);
+let multiplication = (one * two);
+alert(`Умножение: ${multiplication}`);
+let division = (one / two);
+alert(`Деление: ${division}`);
+//Задание 5
+let result = (2 ** 5);
+alert(`Возведение 2 в 5-ю степень: ${result}`);
+
+
