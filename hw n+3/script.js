04e0041db9ba8615f9aafc3a7e15a8d9dfa0bdc2
tatiana <yarova_89@list.ru>
2023-09-14 00:36:11 +0300

    add hw n+3

---
+++ hw n+3/script.js
@@ -0,0 +1,6 @@
+// Задание 1
+let password = '5555';
+let question = prompt(`«Введите пароль»`);
+(password === question) ? alert(`"Пароль введен верно"`) : alert(`"Пароль введен неправильно"`);
+
+
