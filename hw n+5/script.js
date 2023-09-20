6bdc4686b7d0393f53fdb2f5db002dbf16d40226
tatiana <yarova_89@list.ru>
2023-09-21 00:07:56 +0300

    add hw n 5

---
+++ hw n+5/script.js
@@ -0,0 +1,122 @@
+// Задание 1
+function min(a, b) {
+    return a < b ? a: b;
+};
+
+console.log(min(8, 4));
+console.log(min(6, 6));
+
+// Задание 2
+const even = n => !(n % 2);
+
+console.info(even(5));
+console.info(even(4));
+
+// Задание 3.1
+
+let square=(number) =>  number * number;
+ 
+console.log(result = square(5));
+
+// Задание 3.2
+
+// function square(number) { 
+
+//     return number * number; 
+// }; 
+ 
+// console.log(result = square(5));
+
+
+// Задание 4
+
+let age = prompt("Сколько Вам лет?");
+
+
+
+if (age < 0) {
+
+    function printMessage() {
+        console.log("Вы ввели неправильное значение!");
+    }
+
+} else if (age < 12)   {
+
+       function printMessage() {
+       console.log("Привет!");
+
+    }    
+    
+
+} else {
+
+    function printMessage() {
+        console.log("Здравствуйте!");
+    }
+
+}
+
+
+printMessage();
+
+// Задание 5
+const Check = (a, b) => {
+
+    if (isNaN(a) || isNaN(b)){
+
+        return 'Одно или оба значения не являются числом';
+
+    } else {
+        return a * b;
+    }
+}
+console.log(Check("9","9"))
+
+// Задание 6
+let n = Number(prompt('Введите число'));
+
+function numberUser() {
+
+    if (!isNaN(n)) {
+
+        return n ** 3;
+ 
+       
+ 
+    }
+ 
+      else {
+ 
+        console.log(`Переданный параметр не является числом`);
+ 
+      }
+ 
+ }
+ 
+ console.log (numberUser("n"));
+
+ // Задание 7
+
+ function getRectangleArea() {
+    return this.radius * 2 * 3.14;
+}
+function getRectanglePerimiter() {
+   return this.radius * 2 * 3.14; 
+}
+const circle1={ 
+    radius:2,
+
+    getArea: getRectangleArea,
+    getPerimiter: getRectanglePerimiter,
+}
+
+const circle2={ 
+    radius:3,
+
+    getArea: getRectangleArea,
+    getPerimiter: getRectanglePerimiter,
+}
+    console.log(circle1.getArea());
+    console.log(circle1.getPerimiter());
+    console.log(circle2.getArea());
+    console.log(circle2.getPerimiter());
--- script.js
+++ script.js
@@ -1,4 +1,31 @@
 
+const season = () => {
+
+    let userAnswer = Number(prompt(`Введите число меcяца`));
+
+if (userAnswer === 1 || userAnswer === 2 || userAnswer=== 12) {
+       
+        alert('Зима');
+
+} else if (userAnswer === 3 || userAnswer === 4 || userAnswer=== 5) { 
+       
+       alert('Весна');
+
+} else if (userAnswer === 6 || userAnswer === 7 || userAnswer=== 8) {
+       
+        alert('Лето');
+
+}  else if (userAnswer === 9 || userAnswer === 10 || userAnswer=== 11) { 
+       
+        alert('Осень');
+
+} else {
+
+        alert('Неверное значение');
+ 
+} 
+}
+
 
 
 
